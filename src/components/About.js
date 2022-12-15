ca86cc15c777f336c1e77672141281cf1111dd02
Robert Allende <allende.rob@gmail.com>
2022-12-15 11:33:32 -0600

    commit tree 44787ee5c115a46a7b6e4d854367bf09593c56d5

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id = "about">
+    <h2>About Me</h2>
+    <p>Where is this all going? I don't know.</p>
+    <img src = {image} alt = "I made this" />
+  </div>
+  )
 }
 
 export default About;
